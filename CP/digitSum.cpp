c2871a21137939e33a41218a2b641ad2710f50b7
galib <mgalib101@gmail.com>
2023-01-25 18:58:50 +0600

    galib

--- CP/digitSum.cpp
+++ CP/digitSum.cpp
@@ -1,25 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int digitSum(int arr[100],int n){
-        if(n==-1){
-            return 0;
-        }
-       return arr[n]+digitSum(arr,n-1);
+int digitSum(int n){
+    if(n==0) return 0;
 
+    return digitSum(n/10) + n%10;
 
-    }
-
+}
 int main(){
-int arr[100];
+
     int n;
     cin>>n;
-
-    for(int i =0;i<n;++i){
-        cin>>arr[i];
-    }
-
-    cout<<digitSum( arr,n-1);
-    cout<<endl;
-	cout<<"galib"<<endl;
+  cout<<  digitSum(n);
 }
